9f91b8075451cbebdb5c4c9f48d27b58ef828cbd
nguyenevan42 <nguyen_evan42@yahoo.com>
2020-11-06 18:26:39 -0800

    added frame

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import kivy 
+kivy.require("1.11.1")
 
-print('Liquid Hacks 2020')
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+
+    print('Liquid Hacks 2020')
+
+    def build(self):
+        return Label(text="Hello World")
+
+if __name__ == '__main__':
+    MyApp().run()
+
